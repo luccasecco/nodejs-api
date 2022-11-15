cb136bb612c4db792c4c70087b5af200680e804f
luccasecco <luccasecco@hotmail.com>
2022-11-15 19:46:40 -0300

    node api

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import path from 'node:path'
+import { router } from './router'
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express()
+    const port = 3001
+
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+    app.use(express.json())
+    app.use(router)
+
+    app.listen(3001, () => {
+      console.log(`🚀 Server is running on port http://localhost:${port}`)
+    })
+  })
+  .catch(() => console.log('Erro ao conectar no mongo'))
---
+++ src/router.ts
@@ -0,0 +1,53 @@
+import { Router } from 'express'
+import multer from 'multer'
+import path from 'node:path'
+
+import { createCategory } from './app/useCases/categories/createCategoty'
+import { listCategories } from './app/useCases/categories/listCategories'
+import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory'
+import { cancelOrder } from './app/useCases/orders/cancelOrder'
+import { changeOrderStatus } from './app/useCases/orders/changeOrderStatus'
+import { createOrder } from './app/useCases/orders/createOrder'
+import { listOrders } from './app/useCases/orders/listOrders'
+import { createProduct } from './app/useCases/products/createProduct'
+import { listProducts } from './app/useCases/products/listProducts'
+
+export const router = Router()
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, '..', 'uploads'))
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`)
+    }
+  })
+})
+
+// List categories
+router.get('/categories', listCategories)
+
+// Create category list
+router.post('/categories', createCategory)
+
+// List products
+router.get('/products', listProducts)
+
+// Create product
+router.post('/products', upload.single('image'),  createProduct)
+
+// Get prodcts by category
+router.get('/categories/:categoryId/products', listProductsByCategory)
+
+// List orders
+router.get('/orders', listOrders)
+
+// Create orders
+router.post('/orders', createOrder)
+
+// Change order status
+router.patch('/orders/:orderId', changeOrderStatus)
+
+// Delete/cancel order
+router.delete('/orders/:orderId', cancelOrder)
